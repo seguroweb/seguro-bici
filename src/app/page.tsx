892844fd9c53bf118e1aea7914f353a4648e4040
rodriandrade <rodriandrade1498@gmail.com>
2025-07-12 17:34:44 -0300

    Prueba de funcionamiento luego de haber cambiado el puerto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       
-      <h1>Vas a andar o no?</h1>
+      <h1>Vas a andar o no? Cuando dejes el cafe con milanesa</h1>
     </div>
   );
 }
