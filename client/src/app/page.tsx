21a399254a5857da55910831aa8199a0e2a54d69
Ludmila <nannizziludmilaa@gmail.com>
2025-07-16 17:28:08 -0300

    fix yml

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-      <h1>Vas a andar o no? Cuando dejes el cafe con milanesa</h1>
+    <div>
+      <h1>Aguante chaca</h1>
     </div>
   );
 }
